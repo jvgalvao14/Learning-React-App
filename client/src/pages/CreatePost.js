9e3cf3976bd17c65441b2290799e0e9a2765c263
Joao Vitor <jv.galvao14@gmail.com>
2021-05-10 20:55:36 -0300

    Form validation with Formik

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,29 +1,54 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function CreatePost() {
+    const initialValues = {
+        title: "",
+        postText: "",
+        username: "",
+    };
+    const validationSchema = Yup.object().shape({
+        title: Yup.string().required("You must type a title!"),
+        postText: Yup.string().required("You must type a post!"),
+        username: Yup.string()
+            .min(3)
+            .max(15)
+            .required("You must type a username!"),
+    });
+    const onSubmit = (data) => {
+        console.log(data);
+    };
     return (
         <div className="createPostPage">
-            <Formik>
-                <Form>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}
+            >
+                <Form className="formContainer">
                     <label htmlFor="title">Title: </label>
+                    <ErrorMessage name="title" component="span" />
                     <Field
                         id="inputCreatePost"
                         name="title"
                         placeholder="Limão é muito bom!"
                     />
-                    <label htmlFor="title">Post: </label>
+                    <label htmlFor="postText">Post: </label>
+                    <ErrorMessage name="postText" component="span" />
                     <Field
                         id="inputCreatePost"
                         name="postText"
                         placeholder="Tortas de limão são a melhor sobremesa!"
                     />
-                    <label htmlFor="title">Username: </label>
+                    <label htmlFor="username">Username: </label>
+                    <ErrorMessage name="username" component="span" />
                     <Field
                         id="inputCreatePost"
                         name="username"
                         placeholder="Jaolimao203"
                     />
+                    <button type="submit">Create Post</button>
                 </Form>
             </Formik>
         </div>
