51b14bed49c1b24c18c6fef17aa01364495d7963
Joao Vitor <jv.galvao14@gmail.com>
2021-05-10 20:18:40 -0300

    CreatePost form creation

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 function CreatePost() {
     return (
-        <div>
-            <input type="text" />
+        <div className="createPostPage">
+            <Formik>
+                <Form>
+                    <label htmlFor="title">Title: </label>
+                    <Field
+                        id="inputCreatePost"
+                        name="title"
+                        placeholder="Limão é muito bom!"
+                    />
+                    <label htmlFor="title">Post: </label>
+                    <Field
+                        id="inputCreatePost"
+                        name="postText"
+                        placeholder="Tortas de limão são a melhor sobremesa!"
+                    />
+                    <label htmlFor="title">Username: </label>
+                    <Field
+                        id="inputCreatePost"
+                        name="username"
+                        placeholder="Jaolimao203"
+                    />
+                </Form>
+            </Formik>
         </div>
     );
 }
